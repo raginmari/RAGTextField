c44fe823e4404cb54c41ee098fff59fcfdf66b0e
Reimar Twelker <Twelker@adesso-mobile.de>
2017-03-03 11:44:34 +0100

    Repository initialization: Initial commit

---
+++ Example/RAGTextField/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RAGTextField
+//
+//  Created by raginmari on 03/03/2017.
+//  Copyright (c) 2017 raginmari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
