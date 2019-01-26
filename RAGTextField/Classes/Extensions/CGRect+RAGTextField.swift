9dbcf050423006fe1d3c6e43447577c2a29eda84
Reimar Twelker <twelker@adesso-mobile.de>
2019-01-27 00:40:46 +0100

    placeholder-transform: Added CGRect extension.

---
+++ RAGTextField/Classes/Extensions/CGRect+RAGTextField.swift
@@ -0,0 +1,16 @@
+//
+//  CGRect+RAGTextField.swift
+//  Pods-RAGTextField_Example
+//
+//  Created by Reimar Twelker on 27.01.19.
+//
+
+import Foundation
+
+extension CGRect {
+    
+    var center: CGPoint {
+        
+        return CGPoint(x: midX, y: midY)
+    }
+}
